dd5d2d14f18f502d2345ef9225f9d87c2d9ac130
Ramiro Martínez D'Elía <ramiro.m@geopagos.com>
2022-10-23 16:56:50 -0300

    Initial controller

---
+++ src/main/java/com/example/fundingsourcesservice/controller/FundingSourceController.java
@@ -0,0 +1,29 @@
+package com.example.fundingsourcesservice.controller;
+
+import com.example.fundingsourcesservice.data.SampleResponseDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("sources")
+public class FundingSourceController {
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public SampleResponseDto getSource(@PathVariable int id) {
+        return new SampleResponseDto("Get a detail for source: " + id);
+    }
+
+    @GetMapping(value = "/", produces = "application/json")
+    public SampleResponseDto getSources() {
+        return new SampleResponseDto("Listing all sources ...");
+    }
+
+    @PostMapping(value="/", produces = "application/json")
+    public SampleResponseDto createSource() {
+        return new SampleResponseDto("Source created ...");
+    }
+
+    @DeleteMapping(value="/{id}", produces = "application/json")
+    public SampleResponseDto deleteSource(@PathVariable int id) {
+        return new SampleResponseDto("Delete source: " + id);
+    }
+}
---
+++ src/main/java/com/example/fundingsourcesservice/data/SampleResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.fundingsourcesservice.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SampleResponseDto {
+
+    private String message;
+}
